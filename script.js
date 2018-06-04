0a006620fdcc9d141f25ef47d17c6beca80b37c6
Atndesign <atnnetworkk@gmail.com>
2018-06-04 22:45:26 +0200

    V2

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-var inputField = document.getElementById("input")
+//Initial value
 
-inputField.innerHTML = localStorage.getItem("username")
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.getElementById('input');
+const li = document.querySelector('li')
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'))
 
-function getInput() {
-    var input = document.getElementById("input").value;
-    localStorage.setItem("username", input);
-    console.log(input);
+//function that create the LI
+
+const liMaker = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li)
 }
 
+function del() {
+    localStorage.clear();
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+};
+
+//Get input
+
+function getInput() {
+      if (input.value == "") {
+        alert("you must enter something");
+    } else {
+
+
+        itemsArray.push(input.value);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        liMaker(input.value);
+        input.value = "";
+    }
+
+};
+
+data.forEach(item => {
+    liMaker(item);
+})
